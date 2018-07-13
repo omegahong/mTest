8f8bde08e6147cb709eddf5bb3a30260fe94d186
volcanohong <volcanohong@gmail.com>
2018-07-13 12:42:30 -0700

    init

---
+++ src/main/java/com/omega/mtest/MTestApplication.java
@@ -0,0 +1,12 @@
+package com.omega.mtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/omega/mtest/MTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.omega.mtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
